012d515fa35eca3082ed825c3c29524b163ae1ad
Max <titantbx1989215@gmail.com>
2015-03-17 16:39:55 +1300

    minor update

--- experiment_result/plotBest.R
+++ experiment_result/plotBest.R
@@ -1,34 +1,45 @@
 plotme <- function(condition, number = 3){
 	if(number == 3){
 		filename <- paste("3_3", "/", condition, "/", number, ".csv", sep = "")
+		initialisation_filename <- paste("3_3", "/", condition, "/", number, "_initialisation.csv", sep = "")
 	}
 	else if(number == 5){
 		filename <- paste("5_5", "/", condition, "/", number, ".csv", sep = "")
+		initialisation_filename <- paste("5_5", "/", condition, "/", number, "_initialisation.csv", sep = "")
 	}
 	else if(number == 10){
 		filename <- paste("10_10", "/", condition, "/", number, ".csv", sep = "")
+		initialisation_filename <- paste("10_10", "/", condition, "/", number, "_initialisation.csv", sep = "")
 	}
 	else if(number == 15){
 		filename <- paste("15_15", "/", condition, "/", number, ".csv", sep = "")
+		initialisation_filename <- paste("15_15", "/", condition, "/", number, "_initialisation.csv", sep = "")
 	}
 	else if(number == 20){
 		filename <- paste("20_20", "/", condition, "/", number, ".csv", sep = "")
+		initialisation_filename <- paste("20_20", "/", condition, "/", number, "_initialisation.csv", sep = "")
 	}
 	nsga_data <- read_nsga_data(filename)
 	ga_data <- read_ga_data(filename)
-	left_x <- min(c(min(nsga_data$costF), min(ga_data$costF)))
-	right_x <- max(c(max(nsga_data$costF), max(ga_data$costF)))
-	left_y <- min(c(min(nsga_data$latencyF), min(ga_data$latencyF)))
-	right_y <- max(c(max(nsga_data$latencyF), max(ga_data$latencyF)))
+	initialisation_nsga_data <- read_nsga_data(initialisation_filename)
+	initialisation_ga_data <- read_ga_data(initialisation_filename)
+	#left_x <- min(c(min(nsga_data$costF), min(ga_data$costF)))
+	#right_x <- max(c(max(nsga_data$costF), max(ga_data$costF)))
+	#left_y <- min(c(min(nsga_data$latencyF), min(ga_data$latencyF)))
+	#right_y <- max(c(max(nsga_data$latencyF), max(ga_data$latencyF)))
 	par(font.axis = 2)
 	#plot(nsga_data[order(nsga_data$latencyF),], xlim = c(left_x, right_x), ylim = c(left_y, right_y), col = 'blue', xlab = 'cost', ylab = 'latency',  pch = 20, type = 'b', font.lab = 2)
-	plot(nsga_data[order(nsga_data$latencyF),], xlim = c(0, 1), ylim = c(0, 1), col = 'blue', xlab = 'cost', ylab = 'latency',  pch = 20, type = 'b', font.lab = 2)
+	plot(nsga_data[order(nsga_data$latencyF),], xlim = c(0, 0.5), ylim = c(0, 0.7), col = 'blue', xlab = 'cost', ylab = 'latency',  pch = 15, type = 'b', font.lab = 2)
 	par(new = T)
 	#plot(ga_data, xlim = c(left_x,right_x), ylim = c(left_y, right_y), col = 'red', xlab = '', ylab = '', pch = 4)
-	plot(ga_data, xlim = c(0, 1), ylim = c(0, 1), col = 'red', xlab = '', ylab = '', pch = 4)
+	plot(ga_data, xlim = c(0, 0.5), ylim = c(0, 0.7), col = 'red', xlab = '', ylab = '', pch = 1)
+	par(new = T)
+	plot(initialisation_nsga_data[order(initialisation_nsga_data$latencyF),], xlim = c(0, 0.5), ylim = c(0, 0.7), col = 'green', xlab = '', ylab = '', pch = 17, type = 'b')
+	par(new = T)
+	plot(initialisation_ga_data, xlim = c(0, 0.5), ylim = c(0, 0.7), col = 'black', xlab = '', ylab = '', pch = 4)
 
 	par(xpd = T)
-	legend("topright",c("NSGA-II", "GA"), col = c("blue", "red"), pch = c(20, 4))
+	legend("topright",c("NSGA-II", "GA", "Initialized NSGA", "Initialized GA"), col = c("blue", "red", "green", "black"), pch = c(15, 1, 17, 4))
 
 }
 
